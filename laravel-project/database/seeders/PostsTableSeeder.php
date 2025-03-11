6dc6bfe8131062a884605f21e14703a84ccaa1b4
Kazuhiro Sato <sato@gp1000.jp>
2025-03-11 16:37:54 +0900

    PostFactoryの作成、Seeder変更

--- laravel-project/database/seeders/PostsTableSeeder.php
+++ laravel-project/database/seeders/PostsTableSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
-use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,22 +13,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::create([
-            'name' => '鈴木イチロウ',
-            'date' => Carbon::parse('2025-03-01 10:00:00'),
-            'description' => 'Description for 鈴木イチロウ',
-        ]);
-
-        Post::create([
-            'name' => '山田タロウ',
-            'date' => Carbon::parse('2025-03-02 09:00:00'),
-            'description' => 'Description for 山田タロウ',
-        ]);
-
-        Post::create([
-            'name' => '伊藤シンジ',
-            'date' => Carbon::parse('2025-03-03 11:00:00'),
-            'description' => 'Description for 伊藤シンジ',
-        ]);
+        Post::factory(30)->create();
     }
 }
